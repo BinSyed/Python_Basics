38c8a0f7f1d4502c42a50708b7fa431eff9b53e5
BinSyed <70686353+BinSyed@users.noreply.github.com>
2020-09-03 11:12:52 +0400

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+# python is very simplest programming languages compare to others
+# In this dmeo code - we will learn how to print the string using the print statement
+
+print("hello_world_from_python_code")
+
+#thats it, you are done
